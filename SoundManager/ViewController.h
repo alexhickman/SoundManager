11a73d8ba5ebb9587cee2464a005e2d1b16ac101
alexhickman <alex.hickman3@gmail.com>
2016-01-14 12:32:52 -0800

    Initial Commit

---
+++ SoundManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundManager
+//
+//  Created by Hickman on 1/14/16.
+//  Copyright © 2016 Hickman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
